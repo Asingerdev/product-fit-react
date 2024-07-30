0448e474c4eb3256c654b06e89fd7d4042f0748b
Andrew Singer <andrew.singer7@gmail.com>
2024-07-30 10:31:41 -0700

    remove react ga

--- src/App.js
+++ src/App.js
@@ -2,20 +2,8 @@ import Quiz from './components/Quiz'
 import { useEffect } from "react"
 import { FormProvider } from './context/FormContext'
 import './index.css'
-import ReactGA from "react-ga4";
-const TRACKING_ID = process.env.REACT_APP_GA_MEASUREMENT_ID
 
 function App() { 
-
-  useEffect(() => {
-    ReactGA.initialize(TRACKING_ID);
-
-    ReactGA.event({
-      category: 'User',
-      action: 'begin_quiz'
-  });
-}, [])
-
   return (
     <FormProvider>
       <Quiz />
